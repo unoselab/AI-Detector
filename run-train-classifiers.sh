50b0cfb51452c78e5dbe514c848ba446ba738cb7
Song <song@example.com>
2026-05-25 18:54:57 -0500

    2026-05-25 18:54
    Updated

--- run-train-classifiers.sh
+++ run-train-classifiers.sh
@@ -1,8 +1,22 @@
+# Step 1
+# ---
+# cd ~/project-workspace/ai_detector/src
+
+# INPUT_FILE="output/CodeSearchNet/starcoder2-15b-instruct-v0.1-5000-tp0.2/outputs-512token.txt" \
+# DATA_OUT_DIR="code-analyzer-tree-sitter/data_codesearchnet/starcoder2-15b-instruct-v0.1/validsyntax_5000" \
+# N_SMALL=400 \
+# N_LARGE=4500 \
+# SEED=42 \
+# ./run0b-find-validsyntax-mgc.sh
+
+# Step 2
+# ---
 cd ~/project-workspace/ai_detector/src
 
-INPUT_FILE="output/CodeSearchNet/starcoder2-15b-instruct-v0.1-5000-tp0.2/outputs-512token.txt" \
-DATA_OUT_DIR="code-analyzer-tree-sitter/data_codesearchnet/starcoder2-15b-instruct-v0.1/validsyntax_5000" \
-N_SMALL=400 \
-N_LARGE=4500 \
-SEED=42 \
-./run0b-find-validsyntax-mgc.sh
+MODEL_NAME="starcoder2-15b-instruct-v0.1"
+PREFIX="codesearchnet_${MODEL_NAME}_python"
+
+INPUT_CSV="code-analyzer-tree-sitter/data_codesearchnet/${MODEL_NAME}/validsyntax_5000/${PREFIX}_merged_4500.csv" \
+OUT_DIR="code-analyzer-tree-sitter/data_codesearchnet/${MODEL_NAME}/validsyntax_4500_complexity" \
+SIZES="4500" \
+./run0d-build-complexity-sweep-pairs.sh
