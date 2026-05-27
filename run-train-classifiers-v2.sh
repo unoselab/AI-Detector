9cf6333ff30316a58a9ede1f29846f89f3a99f77
dreamxia329 <dreamxia329@gmail.com>
2026-05-27 16:50:23 -0500

    2026-05-27 16:50
    Updated

--- run-train-classifiers-v2.sh
+++ run-train-classifiers-v2.sh
@@ -1,21 +1,23 @@
 # Step 0
 # ---
-cd ~/project-workspace/ai_detector/src
-
-GEN_MAX_NUM=7000 \
-GEN_TEMPERATURE=0.0 \
-GEN_MODEL="gpt-oss" \
-./run0a-generate-llm-api.sh
-
+# cd ~/project-workspace/ai_detector/src
+# 
+# GEN_MAX_NUM=7000 \
+# GEN_TEMPERATURE=0.0 \
+# GEN_MODEL="gpt-oss" \
+# ./run0a-generate-llm-api.sh
+# ==============================================
 # Step 1 
 # ---
-# MODEL_NAME="gpt-oss" \
-# MODEL_DIR="output/CodeSearchNet/gpt-oss-7000-tp0.0" \
-# INPUT_FILE="output/CodeSearchNet/gpt-oss-7000-tp0.0/outputs-512token.txt" \
-# PREFIX="codesearchnet_gpt-oss_python" \
-# DATA_OUT_DIR="code-analyzer-tree-sitter/data_codesearchnet/gpt-oss/validsyntax" \
-# N_SMALL=400 \
-# N_LARGE=4500 \
-# SEED=42 \
-# bash run0b-find-validsyntax-mgc.sh
+cd ~/project-workspace/ai_detector/src
+MODEL_NAME="gpt-oss" \
+MODEL_DIR="output/CodeSearchNet/gpt-oss-7000-tp0.0" \
+INPUT_FILE="output/CodeSearchNet/gpt-oss-7000-tp0.0/outputs-512token.txt" \
+PREFIX="codesearchnet_gpt-oss_python" \
+DATA_OUT_DIR="code-analyzer-tree-sitter/data_codesearchnet/gpt-oss/validsyntax" \
+N_SMALL=400 \
+N_LARGE=4500 \
+SEED=42 \
+bash run0b-find-validsyntax-mgc.sh
+# ==============================================
 
